45ce41310dfbc68a19f02b440784418daed3160e
TomMahe <mahettom@gmail.com>
2023-07-08 14:16:38 +0200

    providing choosen city info to the api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import { optionType } from "./types"
 
 
@@ -6,8 +6,10 @@ import { optionType } from "./types"
 
 const App = (): JSX.Element => {
 
-  const [options, SetOptions] = useState<[]>([])
   const [term, setTerm] = useState<string>('')
+  const [options, SetOptions] = useState<[]>([])
+  const [city, setCity] = useState<optionType | null>(null)
+
 
 
   const getSearchOptions = (value: string) => {
@@ -29,9 +31,23 @@ const App = (): JSX.Element => {
     getSearchOptions(value)
   }
 
-  const onOptionSelect = (Option: {}) => {
+  const onOptionSelect = (option: optionType) => {
+    setCity(option)
 
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${option.lat}&lon=${option.lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+    )
+      .then(res => res.json())
+      .then(data => console.log({ data }))
   }
+
+  useEffect(() => {
+
+    if (city) {
+      setTerm(city.name)
+      SetOptions([])
+    }
+  }, [city])
 
   return (
     <main className='flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full'>
@@ -56,7 +72,7 @@ const App = (): JSX.Element => {
 
           <ul className='absolute top-11 bg-white ml-1 rounded-b-md'>
 
-            {options.map((option: { name: string }, index: number) => (
+            {options.map((option: optionType, index: number) => (
               <li key={option.name + '-' + index}>
                 <button className='text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer' onClick={() => onOptionSelect(option)}>{option.name}</button>
               </li>
